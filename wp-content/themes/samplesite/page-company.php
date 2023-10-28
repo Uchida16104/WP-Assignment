ba5d93f57a3ffd4764d7b8c8f91be862e8dad985
Hirotoshi Uchida <uho02741358@gmail.com>
2023-10-28 12:18:33 +0900

    optimize contents for submission 20231028

--- wp-content/themes/samplesite/page-company.php
+++ wp-content/themes/samplesite/page-company.php
@@ -1,59 +1,79 @@
 <head>
-<meta charset="UTF-8">
-<?php wp_head(); ?>
+    <meta charset="UTF-8">
+    <?php wp_head(); ?>
 </head>
+
 <body>
-<?php get_header(); ?>
-<div id="slide">
+    <?php get_header(); ?>
+    <div id="slide">
         <ul class="slide-inner">
             <li></li>
             <li></li>
             <li></li>
         </ul>
-         <div class="s-prev"><img src="<?php echo get_template_directory_uri(); ?>/images/nav_prev.png" alt="前へ"></div>
-         <div class="s-next"><img src="<?php echo get_template_directory_uri(); ?>/images/nav_next.png" alt="次へ"></div>
-         <div class="cont-nav"></div>
+        <div class="s-prev"><img src="<?php echo get_template_directory_uri(); ?>/images/nav_prev.png" alt="前へ"></div>
+        <div class="s-next"><img src="<?php echo get_template_directory_uri(); ?>/images/nav_next.png" alt="次へ"></div>
+        <div class="cont-nav"></div>
     </div>
-<?php
-/* Template Name: 会社概要 */
-?>
-    <div class="">
-        <?php if(function_exists('bcn_display'))
-        {
-            bcn_display();
-            }?>
+    <?php
+    /* Template Name: 会社概要 */
+    ?>
+    <div id="cont_first" class="container">
+        <div id="contents" class="single">
+            <div class="main">
+                <div class="">
+                    <?php if (function_exists('bcn_display')) {
+                        bcn_display();
+                    } ?>
+                </div>
+                <table>
+                    <tr>
+                        <th>会社名</th>
+                        <td>
+                            <?php the_field('add_company'); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>本社</th>
+                        <td>〒
+                            <?php the_field('add_postnumber'); ?>
+                            <br>
+                            <?php the_field('add_locate'); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>設立</th>
+                        <td>
+                            <?php the_field('add_date'); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>資本金</th>
+                        <td>
+                            <?php the_field('add_money'); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>従業員数</th>
+                        <td>
+                            <?php the_field('add_member'); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>電話番号</th>
+                        <td>
+                            <?php the_field('add_tel'); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>代表者</th>
+                        <td>
+                            <?php the_field('add_name'); ?>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
-<table>
-    <tr>
-    	<th>会社名</th>
-        <td><?php the_field('add_company');?></td>
-    </tr>
-    <tr>
-        <th>本社</th>
-        <td>〒<?php the_field('add_postnumber');?>
-        <br><?php the_field('add_locate');?>
-        </td>
-    </tr>
-    <tr>
-        <th>設立</th>
-        <td><?php the_field('add_date');?></td>
-    </tr>
-    <tr>
-        <th>資本金</th>
-        <td><?php the_field('add_money');?></td>
-    </tr>
-    <tr>
-        <th>従業員数</th>
-        <td><?php the_field('add_member');?></td>
-    </tr>
-    <tr>
-    	<th>電話番号</th>
-    	<td><?php the_field('add_tel');?></td>
-    </tr>
-    <tr>
-    	<th>代表者</th>
-    	<td><?php the_field('add_name');?></td>
-    </tr>
-</table>
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
 </body>
--- wp-content/themes/samplesite/single.php
+++ wp-content/themes/samplesite/single.php
@@ -7,71 +7,28 @@
 <style>
     body{
         font-weight:bold;
-        background-color: white;
-        animation: bg-color 3s infinite;
+        width: 100%;
+        height: 100vh;
+        background: linear-gradient(90deg, #fff0f0,#f0fff0,#f0f0ff);
+        background-size: 200% 200%;
+        animation: bg-color 20s ease infinite;
     }
     @keyframes bg-color {
-        0% { background: repeating-linear-gradient(#ffffffff, #ffffffee 100px, #ffffffdd 100px, #ffffffcc 125px); }
-        6.25% { background: repeating-linear-gradient(#ffffffee, #ffffffdd 100px, #ffffffcc 100px, #ffffffff 125px); }
-        12.5% { background: repeating-linear-gradient(#ffffffdd, #ffffffcc 100px, #ffffffff 100px, #ffffffee 125px); }
-        18.75% { background: repeating-linear-gradient(#ffffffff, #ffffffee 100px, #ffffffdd 100px, #ffffffcc 125px); }
-        25% { background: repeating-linear-gradient(#ffffffee, #ffffffdd 100px, #ffffffcc 100px, #ffffffff 125px); }
-        31.25% { background: repeating-linear-gradient(#ffffffdd, #ffffffcc 100px, #ffffffff 100px, #ffffffee 125px); }
-        37.5% { background: repeating-linear-gradient(#ffffffff, #ffffffee 100px, #ffffffdd 100px, #ffffffcc 125px); }
-        43.75% { background: repeating-linear-gradient(#ffffffee, #ffffffdd 100px, #ffffffcc 100px, #ffffffff 125px); }
-        50% { background: repeating-linear-gradient(#ffffffdd, #ffffffcc 100px, #ffffffff 100px, #ffffffee 125px); }
-        56.25% { background: repeating-linear-gradient(#ffffffff, #ffffffee 100px, #ffffffdd 100px, #ffffffcc 125px); }
-        62.5% { background: repeating-linear-gradient(#ffffffee, #ffffffdd 100px, #ffffffcc 100px, #ffffffff 125px); }
-        68.75% { background: repeating-linear-gradient(#ffffffdd, #ffffffcc 100px, #ffffffff 100px, #ffffffee 125px); }
-        75% { background: repeating-linear-gradient(#ffffffff, #ffffffee 100px, #ffffffdd 100px, #ffffffcc 125px); }
-        81.25% { background: repeating-linear-gradient(#ffffffee, #ffffffdd 100px, #ffffffcc 100px, #ffffffff 125px); }
-        87.5% { background: repeating-linear-gradient(#ffffffdd, #ffffffcc 100px, #ffffffff 100px, #ffffffee 125px); }
-        93.75% { background: repeating-linear-gradient(#ffffffff, #ffffffee 100px, #ffffffdd 100px, #ffffffcc 125px); }
-        96.875% { background: repeating-linear-gradient(#ffffffee, #ffffffdd 100px, #ffffffcc 100px, #ffffffff 125px); }
-        100% { background: repeating-linear-gradient(#ffffffdd, #ffffffcc 100px, #ffffffff 100px, #ffffffee 125px); }
+        0%{background-position:0% 50%;}
+        50%{background-position:100% 50%;}
+        100%{background-position:0% 50%;}
     }
 </style>
 <?php get_header(); ?>
-<div id="slide">
-        <ul class="slide-inner">
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-         <div class="s-prev"><img src="<?php echo get_template_directory_uri(); ?>/images/nav_prev.png" alt="前へ"></div>
-         <div class="s-next"><img src="<?php echo get_template_directory_uri(); ?>/images/nav_next.png" alt="次へ"></div>
-         <div class="cont-nav"></div>
-</div>
-<div id="cont_first" class="container">
-        <div id="contents" class="single">
-        <div class="main">
-        <div class="">
-            <?php if(function_exists('bcn_display')) 
-            {
-                bcn_display();
-                }?>
-        </div>
-        <?php if(have_posts()): while(have_posts()): the_post();?>
-        <?php the_time('Y/m/d'); ?>
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-        <?php endwhile; endif; ?>
-        </div>
-        <?php get_sidebar(); ?>
-    </div>
-</div>
-<?php the_tags(); ?>
-<?php
-$terms = get_the_terms($post->ID,array('language','programming'));
-foreach( $terms as $term ) {
-    echo ",".$term->name;
-}
-?>
-<?php get_footer(); ?>
-</body>
 <?php else : ?>
 <body>
+<style>
+    body{
+        background: linear-gradient(90deg, #ffefef, transparent);
+    }
+</style>
 <?php get_header(); ?>
+<?php endif; ?>
 <div id="slide">
         <ul class="slide-inner">
             <li></li>
@@ -83,19 +40,19 @@ foreach( $terms as $term ) {
          <div class="cont-nav"></div>
 </div>
 <div id="cont_first" class="container">
-        <div id="contents" class="single">
+    <div id="contents" class="single">
         <div class="main">
-        <div class="">
+            <div class="">
             <?php if(function_exists('bcn_display')) 
             {
-                bcn_display();
-                }?>
-        </div>
-        <?php if(have_posts()): while(have_posts()): the_post();?>
-        <?php the_time('Y/m/d'); ?>
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-        <?php endwhile; endif; ?>
+            bcn_display();
+            }?>
+            </div>
+            <?php if(have_posts()): while(have_posts()): the_post();?>
+            <?php the_time('Y/m/d'); ?>
+            <h2><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+            <?php endwhile; endif; ?>
         </div>
         <?php get_sidebar(); ?>
     </div>
@@ -109,4 +66,3 @@ foreach( $terms as $term ) {
 ?>
 <?php get_footer(); ?>
 </body>
-<?php endif; ?>

75683daf17e59b6810134f55a2a92a14b62b20cb
Hirotoshi Uchida <uho02741358@gmail.com>
2023-10-30 13:49:40 +0900

    optimize contents for submission 20231030 02

--- wp-content/themes/samplesite/category-blog.php
+++ wp-content/themes/samplesite/category-blog.php
@@ -9,21 +9,24 @@
         </div>
         <div id="contents">
             <div id="cont_left">
-                <div class="information">
-                    <h2>Blog</h2>
-                    <dl>
+            <div class="information">
+                    <h2>BLOG</h2>
+                    <dl class="blog">
                         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                            <dt><?php the_time('Y-m-d'); ?></dt>
-                            <dd>
-                                <div class="b_img"><?php the_post_thumbnail('thumbside'); ?></div>
-                                <div class="b_right">
-                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                </div>
-                            </dd>
+                        <dt><?php the_time('Y-m-d'); ?></dt>
+                        <dd>
+                        <div class="b_img">
+                        <?php if (has_post_thumbnail()) : ?>
+                            <?php the_post_thumbnail(); ?>
+                            <?php else: ?>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/sample.jpg">
+                        <?php endif; ?>
+                        </div>
+                        <div class="b_right"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div></dd>
                         <?php endwhile; endif; ?>
+                        <?php wp_pagenavi(); ?>
                     </dl>
-                </div>
-                <?php wp_pagenavi(); ?>
+            </div>
             </div>
             <?php get_sidebar(); ?>
         </div>
